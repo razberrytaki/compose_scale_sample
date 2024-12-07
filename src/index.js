24788387b465c91ce8542b47359e45a4737e1fb7
razberrytaki <14997359+razberrytaki@users.noreply.github.com>
2024-12-08 04:42:30 +0900

    fix: indentation fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express() // We are using Express.js for this sample.
 const UUID = randomUUID() // Generate UUID to identify the container.
 
 app.get('/', (req, res) => {
-	res.send(`Hello World! from ${UUID}`)
+  res.send(`Hello World! from ${UUID}`)
 })
 
 export { app, UUID }
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { app, UUID } from './index.js'
 const PORT = process.env.SERVER_PORT || 3000
 
 app.listen(PORT, () => {
-	console.log('Server is running on port', PORT, 'uuid', UUID)
+  console.log('Server is running on port', PORT, 'uuid', UUID)
 })
 
 process.on('SIGTERM', () => {
